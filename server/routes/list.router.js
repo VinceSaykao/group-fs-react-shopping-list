f84f467b52473ff818c1fae23493f74b932cf48a
Davey-M <davey.meuer@gmail.com>
2022-02-17 13:46:12 -0600

    wip post router

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -1,29 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const pool = require("../modules/pool.js");
 
 // TODO - Add routes here...
-router.post('/', (req, res) => {
-    
-    const sqlText = `
+router.post("/", (req, res) => {
+	const sqlText = `
     INSERT INTO "shopping-list" ("name", "qty", "unit", "purchased")
     VALUES
         ($1, $2, $3, $4);
     `;
-    
-    const [ name, qty, unit, purchased ] = req.body;
-    const sqlOptions = [
-        name,
-        qty,
-        unit,
-        purchased,
-    ]
 
-    pool.query(sqlText, sqlOptions)
-        .then(response => {
-            
-        })
-})
+	const [name, qty, unit, purchased] = req.body;
+	const sqlOptions = [name, qty, unit, purchased];
 
+	pool
+		.query(sqlText, sqlOptions)
+		.then((response) => {
+			console.log("Success from server post route:", response);
+			res.sendStatus(201);
+		})
+		.catch((err) => {
+			console.error("Error from server post route:", err);
+		});
+});
 
 module.exports = router;
