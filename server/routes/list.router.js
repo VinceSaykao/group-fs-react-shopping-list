38bc1faa77eb5ce2fbd7479497e25d8253d0c426
Davey-M <davey.meuer@gmail.com>
2022-02-17 13:43:20 -0600

    start post route

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,27 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
+router.post('/', (req, res) => {
+    
+    const sqlText = `
+    INSERT INTO "shopping-list" ("name", "qty", "unit", "purchased")
+    VALUES
+        ($1, $2, $3, $4);
+    `;
+    
+    const [ name, qty, unit, purchased ] = req.body;
+    const sqlOptions = [
+        name,
+        qty,
+        unit,
+        purchased,
+    ]
+
+    pool.query(sqlText, sqlOptions)
+        .then(response => {
+            
+        })
+})
 
 
 module.exports = router;
