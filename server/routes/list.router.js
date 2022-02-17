ae27ddf672b76620ab35dadf9f92f9b28f6760db
Ben Pedrick <bpedrick@gmail.com>
2022-02-17 14:01:40 -0600

    PUT made and tested on serverside

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const pool = require("../modules/pool.js");
 
 // TODO - Add routes here...
 
+// ======================== UPDATE  =====================
+
+router.put("/:id", (req, res) => {
+	let updateItem = req.params.id;
+	let purchaseValue = req.body.purchased;
+	console.log("Updating this id:", updateItem);
+	console.log("This is the value it is changing to", purchaseValue);
+
+	queryParam = `UPDATE "shopping-list" 
+    SET "purchased" = ${purchaseValue}
+    WHERE "id" = ${updateItem};
+    `;
+
+    pool.query(queryParam)
+    .then( result => {
+        console.log(result);
+        res.sendStatus(200);
+    })
+    .catch( err => {
+        console.log('Error on Put',err);
+        res.sendStatus(500);
+    })
+});
 
 module.exports = router;
