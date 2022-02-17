bdd6ae9af9581ac8442a6c2d5a3e8bfc4a5fef92
Ben Pedrick <bpedrick@gmail.com>
2022-02-17 16:49:54 -0600

    Item tested locally with dumb data

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,38 +1,39 @@
 import axios from "axios";
 
 function Item({ listItem, getList }) {
+    
 	// Runs when delete is clicked, should take in the ID for SQL Delete Function
 	const handleDelete = (listItem) => {
-		console.log("Inside Item Delete");
+		console.log("Inside Item Delete", listItem);
 
 		axios
 			.delete(`/list/${listItem}`)
-			.then((res) => {
-				console.log("Deleted, this is response", res);
-				res.sendStatus(200);
+			.then((result) => {
+				console.log("Deleted, this is response", result);
+
+                // 
 				getList();
 			})
 			.catch((err) => {
 				console.log("Error on delete", err);
-				res.sendStatus(500);
+
 			});
 	};
 
 	// Runs when buy is clicked, should take in the ID number for the SQL table
 	const handleBuy = (listItem) => {
-		console.log("Inside Item Buy");
-		let purchased = !listItem.purchased;
+		console.log("Inside Item Buy", listItem);
+		let purchasedFlip = !listItem.purchased;
 
 		axios
-			.put(`/list/${listItem}`, { purchased: purchased })
-			.then((res) => {
-				console.log("Updated purchase, this is response", res);
-				res.sendStatus(200);
+			.put(`/list/${listItem.id}`, { purchased: purchasedFlip })
+			.then((result) => {
+				console.log("Updated purchase, this is response");
+                // Get Updated list, might need rename? 
 				getList();
 			})
-			.catch((err) => {
-				console.log("Error on purchase update", err);
-				res.sendStatus(500);
+			.catch((res) => {
+				console.log("Error on purchase update");
 			});
 	};
 
@@ -44,11 +45,10 @@ function Item({ listItem, getList }) {
 					{listItem.qty} {listItem.unit}
 				</p>
 				{listItem.purchased ? (
-					<button onClick={() => handleBuy(listItem.id)}>Buy</button>
-				) : (
 					<p>Purchased</p>
+				) : (
+					<button onClick={() => handleBuy(listItem)}>Buy</button>
 				)}
-				<button onClick={() => handleBuy(listItem.id)}>Buy</button>
 				<button onClick={() => handleDelete(listItem.id)}>Remove</button>
 			</div>
 		</>
