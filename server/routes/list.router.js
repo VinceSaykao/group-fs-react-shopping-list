b9a2a5c94e800a109106f29f7de310f3d18ac424
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-02-17 14:01:02 -0600

    added get router

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,22 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+// setup GET route to get all shopping-list from database
+router.get('/', (req,res) => {
+    const sqlText= `SELECT * FROM "shopping-list" ORDER BY name DESC;`;
+    pool.query(sqlText)
+    .then((result) => {
+        console.log('GET router works from the database', result);
+        res.sendStatus(500);
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500);
+    })
+})
+
+
+
+
+
 module.exports = router;
